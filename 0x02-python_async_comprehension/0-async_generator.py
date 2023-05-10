ee16348c0d399d8f5e441292f88874749e7203c0
Joel David <106955759+Daveside9@users.noreply.github.com>
2023-05-10 17:14:15 +0300

    Update 0-async_generator.py

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module with an asynchronous generator that yields random numbers."""
+
+import asyncio
+import random
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Module with an asynchronous generator that yields random
+    floating point numbers between 0 and 10."""
+    for Jhay in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
